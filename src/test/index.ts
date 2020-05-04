1c663e3f3ad5d42d645e45d05968a2e013adfc19
dreak603 <dreak603@gmail.com>
2020-05-04 22:37:55 +0800

    small test for bug fix only for lib

---
+++ src/test/index.ts
@@ -0,0 +1,29 @@
+interface Group {
+    objectId: number;
+    name: string;
+}
+
+const test = function (group: Group[] | Group): string {
+    const mapping: string[] = Object.keys(group);
+    let query: string = '';
+    if (mapping[0] === 'objectId') {
+        query = `&objectId=${group[mapping[0]]}`;
+    } else {
+        let i: number = 0;
+        while (i < mapping.length) {
+            query += `$objectId=${group[i].objectId}`;
+            i++;
+        }
+    }
+
+    return query;
+};
+
+const arr = test([
+    { objectId: 1, name: 'test' },
+    { objectId: 2, name: 'test2' },
+]);
+const obj = test({ objectId: 1, name: 'test' });
+
+console.log('_arr', arr);
+console.log('_obj', obj);
