590e7a8013897079a8f61afe751c9e3997332e01
dreak603 <dreak603@gmail.com>
2020-05-05 21:44:49 +0800

    day-five

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
     -   Day 2: [Jewels and Stones](https://github.com/libterty/leetcode-challenge/blob/master/src/day-two/index.ts) :expressionless:
     -   Day 3: [Ransom Note](https://github.com/libterty/leetcode-challenge/blob/master/src/day-three/index.ts) :hushed:
     -   Day 4: [Number Complement](https://github.com/libterty/leetcode-challenge/blob/master/src/day-four/index.ts) :laughing:
+    -   Day 5: [First Unique Character in a String](https://github.com/libterty/leetcode-challenge/blob/master/src/day-five/index.ts) :smirk:
---
+++ src/day-five/index.ts
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+interface MapObject {
+    [key: string]: number;
+}
+
+var firstUniqChar = function (s: string): number {
+    const stringToArr: string[] = s.split('');
+
+    const map: MapObject = stringToArr.reduce((allChar: MapObject, char: string) => {
+        if (char in allChar) {
+            allChar[char]++;
+        } else {
+            allChar[char] = 1;
+        }
+        return allChar;
+    }, {});
+
+    for (let [key, value] of Object.entries(map)) {
+        if (value === 1) {
+            return stringToArr.indexOf(key);
+        }
+    }
+    return -1;
+};
+
+firstUniqChar('leetCode');
